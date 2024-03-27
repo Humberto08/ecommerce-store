51b1b04c61d1d70084e84dd0572eaa4f3ca6b1ff
Humberto Luciano <humberto_luciano@hotmail.com>
2024-03-27 18:31:14 -0300

    feat: Add to Cart functionality (Store)

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -7,10 +7,10 @@ import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
-  const [isMounted, setMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true);
+    setIsMounted(true);
   }, []);
 
   const router = useRouter();
@@ -20,19 +20,19 @@ const NavbarActions = () => {
     return null;
   }
 
-  return (
-    <div className="ml-auto flex ites-center gap-x-4">
-      <Button
-        onClick={() => router.push("/cart")}
-        className="flex items-center rounded-full bg-black px-4 py-2"
-      >
-        <ShoppingBag size={20} color="white" />
-        <span className="ml-2 tect-sm font-medium text-white">
+  return ( 
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag
+          size={20}
+          color="white"
+        />
+        <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
         </span>
       </Button>
     </div>
   );
-};
-
+}
+ 
 export default NavbarActions;
